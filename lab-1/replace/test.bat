85387e7cbd037ba6f305b6017e11439c8ddc539f
egoshin-igor <igor.egoshin@mail.ru>
2018-02-08 23:22:51 +0300

    Убрал выход из программ внутри функций. Сделал замену всех нужных подстрок на другую подстроку.

---
+++ lab-1/replace/test.bat
@@ -0,0 +1,68 @@
+set PROGRAM="%~1"
+
+%PROGRAM% > tests\output.txt
+IF NOT ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\input.txt tests\output.txt abc cb qwer
+IF NOT ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\nonexistentInput.txt tests\output.txt abc wb
+IF NOT ERRORLEVEL 2 GOTO err
+
+%PROGRAM% tests\input.txt tests\lockedOutput.txt abc cab
+IF NOT ERRORLEVEL 3 GOTO err
+
+%PROGRAM% tests\input.txt tests\output.txt "" cab
+IF NOT ERRORLEVEL 4 GOTO err
+
+%PROGRAM% tests\empty.txt tests\output.txt abc cab
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\empty.txt tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\notFound.txt tests\output.txt "If search string not found" "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\notFound.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\mama.txt tests\output.txt ma mama
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\mamaResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\digit.txt tests\output.txt 1231234 "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\digitResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\oneString.txt tests\output.txt all "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\oneStringResultWithPartReplace.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\oneString.txt tests\output.txt "This is all string" "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\oneStringResultWithFullReplace.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\longString.txt tests\output.txt "qwerty" "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\longStringResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\threeLines.txt tests\output.txt qwerty "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\threeLinesResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\bigFile.txt tests\output.txt qwerty "Replacement completed"
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\bigFileResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+ECHO Program testing succeeded :-)
+EXIT
+
+:err
+ECHO Program testing failed :-(
+EXIT
