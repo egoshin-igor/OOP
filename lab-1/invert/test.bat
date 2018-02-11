93b8a2892090c5a8a427d597eb2d01c3a16db5e1
egoshin-igor <igor.egoshin@mail.ru>
2018-02-11 15:44:09 +0300

    Исправил способ запуска тестов и убрал передачу матрицу в функцию с помощью ссылки, тк в любом случае передается адрес на 1 элемент

---
+++ lab-1/invert/test.bat
@@ -0,0 +1,35 @@
+set PROGRAM="%~1"
+
+%PROGRAM%
+IF NOT ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\nonexistentInput.txt
+IF NOT ERRORLEVEL 2 GOTO err
+
+%PROGRAM% tests\incorrectFileContent.txt
+IF NOT ERRORLEVEL 3 GOTO err
+
+%PROGRAM% tests\determinateZero.txt
+IF NOT ERRORLEVEL 4 GOTO err
+
+%PROGRAM% tests\unitMatrix.txt > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\unitMatrixResult.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\example1.txt > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\example1Result.txt
+IF ERRORLEVEL 1 GOTO err
+
+%PROGRAM% tests\example2.txt > tests\output.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B tests\output.txt tests\example2Result.txt
+IF ERRORLEVEL 1 GOTO err
+
+ECHO Program testing succeeded :-)
+EXIT
+
+:err
+ECHO Program testing failed :-(
+EXIT
